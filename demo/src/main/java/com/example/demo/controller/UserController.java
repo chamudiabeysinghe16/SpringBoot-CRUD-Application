260d5f510137222c923d9a4cbc11efa6f60ff3df
chamudiabeysinghe <cabeysinghe16@gmail.com>
2022-12-12 13:32:23 +0530

    Create UserController.java

---
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.UserDTO;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value="api/v1/user")
+@CrossOrigin
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/getUsers")
+    public List<UserDTO> getUser() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/saveUser")
+    public UserDTO saveUser(@RequestBody UserDTO userDTO){ //@RequestBody-Deserialized into Java object
+        return userService.saveUser(userDTO);
+    }
+
+    @PutMapping("/updateUser")
+    public UserDTO updateUser(@RequestBody UserDTO userDTO){
+        return userService.updateUser(userDTO);
+    }
+
+    @DeleteMapping("/deleteUser")
+    public boolean deleteUser(@RequestBody UserDTO userDTO){
+        return userService.deleteUser(userDTO);
+    }
+
+    @GetMapping("/getUserByUserId/{userID}")
+    public UserDTO getUserByUserID(@PathVariable String userID){
+        return userService.getUserByUserID(userID);
+    }
+
+    @GetMapping("/getUserByUserIDAndAddress/{userID}/{address}")
+    public UserDTO getUserByUserIDAndAddress(@PathVariable String userID, @PathVariable String address){
+        System.out.println("User Id: " +userID + "User Address:"+address);
+        return userService.getUserByUserIDAndAddress(userID,address);
+    }
+
+
+}
